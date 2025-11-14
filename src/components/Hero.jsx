6a636641b6b41aff8c1be94c17f331a76aea92a9
Jainish Shah <jainishshah@Mac-17444.lan>
2025-11-13 22:26:58 -0800

    commented canvas and changes to home height

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className='relative w-full h-[60vh] mx-auto'>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Jainish</span>
+            Hi, I'm <span className='text-[#915EFF]'>Jainish Shah</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
           As a current master's student in computer science,<br className='sm:block hidden' />
@@ -25,10 +25,10 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
+        {/* <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
@@ -42,7 +42,7 @@ const Hero = () => {
               className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
-        </a>
+        </a> */}
       </div>
     </section>
   );
